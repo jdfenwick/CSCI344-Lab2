fbb9af3a4870fbfa66e1ab1b3ccead6100d23385
John Fenwick <jfenwic1@unca.edu>
2013-02-19 17:36:14 -0800

    Implementation of fadeOut command

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,13 +1,20 @@
 var main = function () {
   
-    var twitter = new ctwitter.CTwitter();
-    twitter.stream("statuses/filter", { lang:"en", track:["Zurb Foundation", "Twitter Bootstrap"] }, function (stream) {
-        stream.on("data", function (tweet) {
-              $("#tweets").prepend("p class = 'response"+tweet.text+"' </p>");
-              
-
-        });
-    });
+	var twitter = new ctwitter.CTwitter();
+	twitter.stream("statuses/filter", {lang:"en", track:["Zurb Foundation"]}, function(stream) {
+		stream.on("data", function(tweet) {
+      
+			$("#tweets").prepend("<p class='response'>" + tweet.text + "</p>");
+      $(".response").fadeOut(9000, function() {
+        
+				$(this).remove();
+        
+			});
+      
+		});
+    
+	});
+  
 }
 
 
