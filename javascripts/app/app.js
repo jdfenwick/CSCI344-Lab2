f6dc19bd0caf1ec4f529adb3b2272d98ef1c96ee
John Fenwick <jfenwic1@unca.edu>
2013-02-19 17:03:46 -0800

    Cleans up existing work to clean slate and adds webfonts

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,14 +1,7 @@
 var main = function () {
-    console.log("hello world");
   
-    var twitter = new ctwitter.CTwitter();
-    twitter.stream("statuses/filter", { lang:"en", track:["node.js", "javascript"] }, function (stream) {
-        stream.on("data", function (tweet) {
-              $("#tweets").append("<img src='"+tweet.profile_image_url+"' />");
-              console.log(tweet.profile_image_url);
-
-        });
-    });
+  
+    
 }
 
 
